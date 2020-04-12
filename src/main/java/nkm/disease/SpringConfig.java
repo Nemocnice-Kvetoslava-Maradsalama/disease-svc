25dc41530c8db39cae5d3e45623a807de07f730d
Tadeáš Binder <bindetad@fel.cvut.cz>
2020-04-12 17:18:24 +0200

    swagger init

--- src/main/java/nkm/disease/SpringConfig.java
+++ src/main/java/nkm/disease/SpringConfig.java
@@ -11,11 +11,17 @@ import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 import javax.sql.DataSource;
 import java.util.Locale;
 
 @Configuration
+@EnableSwagger2WebMvc
 public class SpringConfig {
     @Autowired
     private Environment env;
@@ -37,6 +43,15 @@ public class SpringConfig {
         SessionLocaleResolver localeResolver = new SessionLocaleResolver();
         localeResolver.setDefaultLocale(new Locale("cs","CZ"));
         return localeResolver;
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
     }
 
     @Bean
