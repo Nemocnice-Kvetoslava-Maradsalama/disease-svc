0170c96153c3d4da7806c383fdb12799f66fa199
Tadeáš Binder <bindetad@fel.cvut.cz>
2020-04-13 14:05:19 +0200

    diagnose endpoint

--- src/main/java/nkm/disease/controller/DiseaseController.java
+++ src/main/java/nkm/disease/controller/DiseaseController.java
@@ -3,10 +3,13 @@ package nkm.disease.controller;
 
 import nkm.disease.model.Disease;
 import nkm.disease.model.Symptom;
+import nkm.disease.model.Symptoms;
 import nkm.disease.service.DiseaseService;
 import nkm.disease.service.SymptomService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
 
 @RestController
 @RequestMapping("/disease")
@@ -31,6 +34,28 @@ public class DiseaseController {
         Disease d = diseaseService.find(disease);
         d.getCures().add(cure);
         diseaseService.update(d);
+    }
+
+    @RequestMapping(value = "/diagnose/", method = RequestMethod.POST)
+    public List<Disease> diagnose(@RequestBody Symptoms symptoms){
+        List<Disease> ds = diseaseService.findAll();
+        TreeMap<Disease, Integer> match = new TreeMap<>(Collections.reverseOrder());
+        List<Symptom> ss = symptoms.getSymptoms();
+        for (Disease d : ds){
+            Integer matches = 0;
+            for (Symptom s : ss){
+                if (d.getSymptoms().contains(s)) matches++;
+            }
+            match.put(d, matches);
+        }
+        List<Disease> result = new ArrayList<>();
+        Iterator<Map.Entry<Disease, Integer>> res = match.entrySet().iterator();
+        for (int i = 0; i < (Math.min(match.entrySet().size(), 10)); i++){
+            Map.Entry<Disease, Integer> entry = res.next();
+            if (entry.getValue() == 0) break;
+            else result.add(entry.getKey());
+        }
+        return result;
     }
 
     @RequestMapping(value = "/", method = RequestMethod.PUT)
---
+++ src/main/java/nkm/disease/model/Symptoms.java
@@ -0,0 +1,15 @@
+package nkm.disease.model;
+
+import java.util.List;
+
+public class Symptoms {
+    private List<Symptom> symptoms;
+
+    public List<Symptom> getSymptoms() {
+        return symptoms;
+    }
+
+    public void setSymptoms(List<Symptom> symptoms) {
+        this.symptoms = symptoms;
+    }
+}
