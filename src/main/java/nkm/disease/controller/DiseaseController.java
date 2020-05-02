5d56bd519ffa2b5dac2942a13d243c77f4e450a1
Tadeáš Binder <bindetad@fel.cvut.cz>
2020-05-02 16:28:00 +0200

    logging

--- src/main/java/nkm/disease/controller/DiseaseController.java
+++ src/main/java/nkm/disease/controller/DiseaseController.java
@@ -7,11 +7,11 @@ import nkm.disease.model.Disease;
 import nkm.disease.model.Symptom;
 import nkm.disease.model.Symptoms;
 import nkm.disease.service.DiseaseService;
-import nkm.disease.service.SymptomService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
+import java.util.logging.Logger;
 
 @RestController
 @RequestMapping("/disease")
@@ -20,21 +20,26 @@ public class DiseaseController {
     @Autowired
     private DiseaseService diseaseService;
 
+    private final static Logger LOGGER = Logger.getLogger(DiseaseController.class.getName());
+
     @ApiOperation(value = "Returns all diseases.")
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public List<Disease> getAllDiseases(){
+        LOGGER.info("Returning all diseases.");
         return diseaseService.findAll();
     }
 
     @ApiOperation(value = "Returns a single disease based on an ID of the disease.")
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public Disease getDisease(@PathVariable @ApiParam(required = true, value = "Disease ID of type Long") Long id){
+        LOGGER.info("Returning disease (id = "+id+").");
         return diseaseService.find(id);
     }
 
     @ApiOperation(value = "Adds a disease into the database.")
     @RequestMapping(value = "/", method = RequestMethod.POST)
     public Long addDisease(@RequestBody @ApiParam(value = "Disease JPA entity to be added", required = true) Disease entity){
+        LOGGER.info("Persisting disease into database.");
         diseaseService.persist(entity);
         return entity.getId();
     }
@@ -42,6 +47,7 @@ public class DiseaseController {
     @ApiOperation(value = "Adds a cure of a certain disease to the database.")
     @RequestMapping(value = "/addCure/", method = RequestMethod.POST)
     public void addCureToDisease(@RequestBody @ApiParam(required = true, value = "Disease ID of type Long") Long disease, @RequestBody @ApiParam(required = true, value = "Cure ID of type Long") Long cure){
+        LOGGER.info("Persisting cure (id = "+cure+") into disease (id = "+disease+").");
         Disease d = diseaseService.find(disease);
         d.getCures().add(cure);
         diseaseService.update(d);
@@ -50,6 +56,8 @@ public class DiseaseController {
     @ApiOperation(value = "Returns an array of at most ten diseases, ordered in descending order by number of symptoms of a given disease that match the list of symptoms given in the parameter.")
     @RequestMapping(value = "/diagnose/", method = RequestMethod.POST)
     public List<Disease> diagnose(@RequestBody @ApiParam(required = true, value="Array of JPA symptom entities") Symptoms symptoms){
+        String stream = symptoms.getSymptoms().stream().map(x -> x.getId().toString()).reduce("", (a,b) -> (a + " " + b));
+        LOGGER.info("Diagnosing symptoms (id(s) = "+ stream +").");
         List<Disease> ds = diseaseService.findAll();
         HashMap<Disease, Integer> match = new HashMap<>();
         List<Symptom> ss = symptoms.getSymptoms();
@@ -76,12 +84,14 @@ public class DiseaseController {
     @ApiOperation(value = "Updates a disease in the database. The parameter disease is matched with a disease in the database based on the IDs.")
     @RequestMapping(value = "/", method = RequestMethod.PUT)
     public void updateDisease(@RequestBody @ApiParam(value = "Disease JPA entity", required = true) Disease entity){
+        LOGGER.info("Updating disease (id = "+entity.getId()+").");
         diseaseService.update(entity);
     }
 
     @ApiOperation(value = "Deletes a single disease based on an ID of the disease.")
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
     public void deleteDisease(@PathVariable @ApiParam(required = true, value = "Disease ID of type Long") Long id){
+        LOGGER.info("Deleting disease (id = "+id+").");
         diseaseService.remove(diseaseService.find(id));
     }
 }
--- src/main/java/nkm/disease/controller/SymptomController.java
+++ src/main/java/nkm/disease/controller/SymptomController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 @RestController
 @RequestMapping("/symptom")
@@ -17,21 +18,26 @@ public class SymptomController {
     @Autowired
     private SymptomService symptomService;
 
+    private final static Logger LOGGER = Logger.getLogger(SymptomController.class.getName());
+
     @ApiOperation(value = "Returns all symptoms.")
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public List<Symptom> getAllSymptoms(){
+        LOGGER.info("Returning all symptoms.");
         return symptomService.findAll();
     }
 
     @ApiOperation(value = "Returns a single symptom based on an ID of the symptom.")
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public Symptom getSymptom(@PathVariable @ApiParam(value = "Symptom ID of type Long", required = true) Long id){
+        LOGGER.info("Returning symptom (id = "+id+")");
         return symptomService.find(id);
     }
 
     @ApiOperation(value = "Adds a symptom into the database.")
     @RequestMapping(value = "/", method = RequestMethod.POST)
     public Long addSymptom(@RequestBody @ApiParam(value = "Symptom JPA entity", required = true) Symptom entity){
+        LOGGER.info("Persisting symptom.");
         symptomService.persist(entity);
         return entity.getId();
     }
@@ -39,12 +45,14 @@ public class SymptomController {
     @ApiOperation(value = "Updates a symptom in the database. The parameter disease is matched with a symptom in the database based on the IDs.")
     @RequestMapping(value = "/", method = RequestMethod.PUT)
     public void updateSymptom(@RequestBody @ApiParam(value = "Symptom JPA entity", required = true) Symptom entity){
+        LOGGER.info("Updating symptom (id = "+entity.getId()+").");
         symptomService.update(entity);
     }
 
     @ApiOperation(value = "Deletes a single symptom based on an ID of the symptom.")
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
     public void deleteSymptom(@PathVariable @ApiParam(value = "Symptom ID of type Long", required = true) Long id){
+        LOGGER.info("Deleting symptom (id = "+id+").");
         symptomService.remove(symptomService.find(id));
     }
 }
