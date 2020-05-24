3c7a5f0c0eb51d67e244f6f6d6fe1198d711b455
Tadeáš Binder <bindetad@fel.cvut.cz>
2020-05-24 15:38:52 +0200

    tests

--- src/test/java/nkm/disease/DiseaseServiceTests.java
+++ src/test/java/nkm/disease/DiseaseServiceTests.java
@@ -1,6 +1,7 @@
 package nkm.disease;
 
 import nkm.disease.model.Disease;
+import nkm.disease.model.Symptom;
 import nkm.disease.service.DiseaseService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
@@ -15,19 +16,54 @@ class DiseaseServiceTests {
     private DiseaseService diseaseService;
 
     @Test
-    void diseaseServiceTest(){
+    void diseaseServiceCreate(){
         Disease disease = new Disease();
         disease.setName("Test disease");
         disease.setIcd10("F64");
         disease.setDescription("Description");
         diseaseService.persist(disease);
-        Assertions.assertTrue(diseaseService.exists(disease.getId()));
-        Disease foundDisease = diseaseService.find(disease.getId());
-        Assertions.assertEquals(foundDisease.getId(), disease.getId());
-        foundDisease.setName("Test disease update");
-        diseaseService.update(foundDisease);
-        List<Disease> diseases = diseaseService.findAll();
-        diseaseService.remove(diseases.get(foundDisease.getId().intValue()));
-        Assertions.assertNull(diseaseService.find(foundDisease.getId()));
+        Assertions.assertTrue(diseaseService.findAll().size() > 0);
+    }
+
+    @Test
+    void diseaseServiceGet(){
+        Disease disease = new Disease();
+        disease.setName("Test disease");
+        disease.setIcd10("F64");
+        disease.setDescription("Description");
+        diseaseService.persist(disease);
+        Disease actual = diseaseService.find(disease.getId());
+        Assertions.assertEquals(actual.getDescription(), disease.getDescription());
+        Assertions.assertEquals(actual.getName(), disease.getName());
+    }
+
+    @Test
+    void diseaseServiceUpdate(){
+        Disease disease = new Disease();
+        disease.setName("Test disease");
+        disease.setIcd10("F64");
+        disease.setDescription("Description");
+        diseaseService.persist(disease);
+        Disease update = new Disease();
+        update.setName("Update disease");
+        update.setDescription("Update description");
+        update.setIcd10("F64.0");
+        update.setId(disease.getId());
+        diseaseService.update(update);
+        Disease actual = diseaseService.find(update.getId());
+        Assertions.assertEquals(actual.getDescription(), update.getDescription());
+        Assertions.assertEquals(actual.getName(), update.getName());
+    }
+
+    @Test
+    void diseaseServiceDelete(){
+        Disease disease = new Disease();
+        disease.setName("Test disease");
+        disease.setIcd10("F64");
+        disease.setDescription("Description");
+        diseaseService.persist(disease);
+        Disease actual = diseaseService.find(disease.getId());
+        diseaseService.remove(actual);
+        Assertions.assertNull(diseaseService.find(actual.getId()));
     }
 }
--- src/test/java/nkm/disease/SymptomServiceTests.java
+++ src/test/java/nkm/disease/SymptomServiceTests.java
@@ -16,18 +16,49 @@ class SymptomServiceTests {
     private SymptomService symptomService;
 
     @Test
-    void symptomServiceTest(){
+    void symptomServiceCreate(){
         Symptom symptom = new Symptom();
         symptom.setName("Test symptom");
         symptom.setDescription("Description");
         symptomService.persist(symptom);
-        Assertions.assertTrue(symptomService.exists(symptom.getId()));
-        Symptom foundSymptom = symptomService.find(symptom.getId());
-        Assertions.assertEquals(foundSymptom.getId(), symptom.getId());
-        foundSymptom.setName("Test symptom update");
-        symptomService.update(foundSymptom);
-        List<Symptom> diseases = symptomService.findAll();
-        symptomService.remove(diseases.get(foundSymptom.getId().intValue()));
-        Assertions.assertNull(symptomService.find(foundSymptom.getId()));
+        Assertions.assertTrue(symptomService.findAll().size() > 0);
+    }
+
+    @Test
+    void symptomServiceGet(){
+        Symptom symptom = new Symptom();
+        symptom.setName("Test symptom");
+        symptom.setDescription("Description");
+        symptomService.persist(symptom);
+        Symptom actual = symptomService.find(symptom.getId());
+        Assertions.assertEquals(actual.getDescription(), symptom.getDescription());
+        Assertions.assertEquals(actual.getName(), symptom.getName());
+    }
+
+    @Test
+    void symptomServiceUpdate(){
+        Symptom symptom = new Symptom();
+        symptom.setName("Test symptom");
+        symptom.setDescription("Description");
+        symptomService.persist(symptom);
+        Symptom update = new Symptom();
+        update.setName("Update symptom");
+        update.setDescription("Update description");
+        update.setId(symptom.getId());
+        symptomService.update(update);
+        Symptom actual = symptomService.find(update.getId());
+        Assertions.assertEquals(actual.getDescription(), update.getDescription());
+        Assertions.assertEquals(actual.getName(), update.getName());
+    }
+
+    @Test
+    void symptomServiceDelete(){
+        Symptom symptom = new Symptom();
+        symptom.setName("Test symptom");
+        symptom.setDescription("Description");
+        symptomService.persist(symptom);
+        Symptom actual = symptomService.find(symptom.getId());
+        symptomService.remove(actual);
+        Assertions.assertNull(symptomService.find(actual.getId()));
     }
 }
